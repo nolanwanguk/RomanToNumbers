2d0d7b67504c61415ca8f8338f6f2ebb8052854f
nolanwanguk <vitanuan@gmail.com>
2022-06-15 10:09:35 +0100

    update solutions

---
+++ Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+
+namespace Test
+{
+
+    public class Tests
+    {
+        private Solver Solver;
+        
+        [SetUp]
+        public void Setup()
+        {
+            Solver = new Solver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(4,Solver.RomanToInt("IV"));
+        }
+    }
+}
